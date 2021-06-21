239233abc0fd4e67d9249f81dbf736b80f1109e4
Moez <moez_zouari@hotmail.fr>
2021-06-21 12:08:21 +0100

    Adding Checkout Form

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -12,7 +12,7 @@ const AvailableMeals = () => {
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://react-http-1e59e-default-rtdb.firebaseio.com/meals"
+        "https://react-http-1e59e-default-rtdb.firebaseio.com/meals.json"
       );
       if (!response.ok) {
         throw new Error("Something Went Wrong !");
